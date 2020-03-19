419af4d057ee56e41425a822a2151632a85adec6
muhamadfrio <muhamadfrio@gmail.com>
2020-03-19 16:17:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
+/* eslint-disable react/style-prop-object */
 import React from 'react';
-import logo from './logo.svg';
+import Iframe from 'react-iframe';
 import './App.css';
+import CloseIcon from '@material-ui/icons/Close';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+import { Hidden } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  fab: {
+    position: 'absolute',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+    background: '#fe4c4c',
+    color: '#ffffff',
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  frameContainer: {
+    borderRadius: 20,
+  }
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div id="rewardsButtonLayout">
+        <Fab variant="extended" className={classes.fab} id="rewardsButton" onClick={function () {
+          document.getElementById("rewardsButtonLayout").style.display = "none";
+          document.getElementById("myForm").style.display = "block";
+          document.getElementById("closeButtonLayout").style.display = "block";
+        }}>
+          <img src="https://cdn.sweettooth.io/v1/images/launcher_icons/crown.svg?color=%23FFFFFF" alt="" className={classes.extendedIcon} />
+        Rewards
+      </Fab>
+      </div>
+      <div className="popup" id="myForm">
+        <div className="form-container">
+          <Iframe url="https://mediadistribution.espn.com/"
+            className={classes.frameContainer}
+            overflow="hidden"
+            height="450px"
+            frameBorder="0"
+            display="initial"
+            position="relative" />
+        </div>
+        <div id="closeButtonLayout" style={{ display: 'none' }} >
+          <Fab className={classes.fab} id="closeButton" onClick={function () {
+            document.getElementById("rewardsButtonLayout").style.display = "block";
+            document.getElementById("myForm").style.display = "none";
+            document.getElementById("closeButtonLayout").style.display = "none";
+          }}>
+            <CloseIcon />
+          </Fab>
+        </div>
+      </div>
     </div>
   );
 }
